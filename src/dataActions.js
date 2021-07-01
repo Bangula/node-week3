2f68bba44d1fc9b9376c0082121423c309737925
Branko Vukovic <branko.vukovic@quantox.com>
2021-07-01 14:40:28 +0200

    get Fast status and write new Fast

--- src/dataActions.js
+++ src/dataActions.js
@@ -1,13 +1,13 @@
 import moment from "moment";
+import chalk from "chalk";
 import fs from "fs";
 
-function readData(){
+export function readData(){
      return new Promise( (resolve, reject) => {
         fs.readFile(`${__dirname}/fasts.json`, 'utf8', function(err, data){
             if (err){
                 reject(new Error(err));
             } else {
-                console.log("json file data: ", data)
                 if(data){
                     let obj = JSON.parse(data); //now it an object
                     let fasts = obj || [];
@@ -15,41 +15,49 @@ function readData(){
                     // json = JSON.stringify(obj); //convert it back to json
                     // fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back 
                     resolve(fasts);
-                }else resolve ([])
-                
+                }else resolve ([])                
             }});
      })
         
 }
 
 export async function checkActiveFasts(){
-    return false;
+    let fasts = await readData();
+    if(fasts.length){
+        let last = fasts[fasts.length - 1];        
+        if(moment(last.end_date).format() > moment().format()) {
+            console.log(fasts[fasts.length - 1]);
+            return true;
+
+        }
+        else return false
+    }
+    else return false;
 }
 
 export async function writeData(values){
+    console.log(values.start_date);    
+    console.log(moment(values.start_date).format())
 
-    let startDate = new Date(values.start_date)      
-    let endDate = new Date(new Date(startDate).setHours(startDate.getHours() + values.fast_type));
-    console.log(startDate, endDate);    
+    let endDate = moment(values.start_date).add(2, 'hours')
  
     try{
         let fasts = await readData();
         let newFast = {            
-            start_date: startDate,
-            fast_type: endDate
+            start_date: moment(values.start_date).format(),
+            fast_type: values.fast_type,
+            end_date: endDate.format()
         }
         fasts.push(newFast);
         let json = JSON.stringify(fasts);
         fs.writeFile(`${__dirname}/fasts.json`, json, 'utf8', (err, data) => {
-            if(err) console.log(err)
-            else console.log(data)
+            if(err) console.log(err);
+            else {
+                console.log(`\n ${chalk.bold.green("The Fast was scheduled successfully.")} \n`);
+            }
         }); 
-
     }
     catch(err){
         console.log(err)
     }
-
-    console.log("write data to json file ", values);
-  
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import chalk from "chalk"
 import datePrompt from "date-prompt";
 import inquirer from "inquirer";
-import { writeData, checkActiveFasts } from "./dataActions"
+import { writeData, checkActiveFasts, readData } from "./dataActions"
 
 const confirmAnswerValidator = async (input) => {
     if (isNaN(input) || Number(input) > 48 || Number(input) < 1) {
@@ -20,8 +20,7 @@ const startFast = () => {
           message: "Fast Type (how many hours will the fast last, example: 16)",
           validate: confirmAnswerValidator
         }
-      ];
-      
+      ];     
       
     datePrompt('Start Date: (When fasting should begin)')
     .then(isoStr => {
@@ -31,21 +30,33 @@ const startFast = () => {
     })
     .catch(isoStr => console.log('Aborted with', isoStr))}
 
-const printFastStatus = () => {
-    console.log(`
-        Status: - ${chalk.green('Active')}
-        Started Fasting - ${chalk.blue('April 2, 20:00')}
-        Fast Ending - ${chalk.blue('April 6, 20:00')}
-        Elapsed Time - ${chalk.blue('1:30:45')}
-        Fast Type - ${chalk.yellow('16 hours')}            
-        `);
+const printFastStatus = async () => {
+    let data = await readData();
+    if(data){
+        if(data.length){
+            let lastFast = data[data.length - 1];
+            let difference = new Date().getTime() - new Date(lastFast.start_date).getTime();
+            let formatedDiff = Math.floor(difference/(1000*60*60)) + ":" + Math.floor(difference/(1000*60))%60 + ":" + Math.floor(difference/1000)%60;
+            console.log(`
+                Fast Status: - ${chalk.green('Active')}
+
+                Started Fasting - ${chalk.blue(lastFast.start_date)}
+
+                Fast Ending - ${chalk.blue(lastFast.end_date)}
+
+                Elapsed Time - ${chalk.blue(formatedDiff)}
+
+                Fast Type - ${chalk.yellow(`${lastFast.fast_type} hours`)} \n            
+                `);
+        }
+    }
+    
 }
 
 export async function processUserAction(options){
     let activeStatus = await checkActiveFasts();
-    console.log("test", activeStatus)
     if(options.action == "status"){
-        if(activeStatus) console.log(`\n ${chalk.bold.blue("There is no active fast, type 'zero start' in terminal to start with fast.")} \n`);
+        if(!activeStatus) console.log(`\n ${chalk.bold.blue("There is no active fast, type 'zero start' in terminal to start with fast.")} \n`);
         else  printFastStatus();
     }
     else if(options.action == "start"){
